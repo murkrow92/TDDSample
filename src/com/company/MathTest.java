196bd4a61f0768a60549d639afdc07f3fd972a84
murkrow92 <ducdt199@gmail.com>
2019-12-14 00:46:49 +0700

    add math test java

--- src/com/company/MathTest.java
+++ src/com/company/MathTest.java
@@ -1,7 +1,24 @@
 package com.company;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MathTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void isPrimaryNumber() {
+        boolean result = Math.isPrimaryNumber(0);
+        assertFalse(result);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
