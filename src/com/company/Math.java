242eb232a354bf8662b7b1a27f4183560d854ef6
murkrow92 <ducdt199@gmail.com>
2019-12-14 21:48:04 +0700

    add test case

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -2,6 +2,45 @@ package com.company;
 
 public class Math {
     public static boolean isPrimaryNumber(int number) {
-        return false;
+        if (number > 1) {
+            for (int i = 2; i < number; i++) {
+                System.out.println("i:" + i);
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public static int exp(int number) {
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result = result * i;
+            System.out.println("=====================");
+            System.out.println("i:" + i);
+            System.out.println("num:" + number);
+            System.out.println("exp:" + result);
+        }
+        return result;
+    }
+
+    public static boolean changeTemperature(int temp) {
+        if (temp > 15 && temp < 33) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean login(String username, String password) {
+        if (username.equalsIgnoreCase("Ducdt@123") && password.equalsIgnoreCase("123456")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/com/company/MathTest.java
+++ src/com/company/MathTest.java
@@ -16,6 +16,84 @@ public class MathTest {
     public void isPrimaryNumber() {
         boolean result = Math.isPrimaryNumber(0);
         assertFalse(result);
+
+        boolean result2 = Math.isPrimaryNumber(1);
+        assertFalse(result2);
+
+        boolean result3 = Math.isPrimaryNumber(2);
+        assertTrue(result3);
+
+        boolean result4 = Math.isPrimaryNumber(3);
+        assertTrue(result4);
+
+        boolean result5 = Math.isPrimaryNumber(4);
+        assertFalse(result5);
+
+        boolean result6 = Math.isPrimaryNumber(5);
+        assertTrue(result6);
+
+        boolean result7 = Math.isPrimaryNumber(6);
+        assertFalse(result7);
+
+        boolean result8 = Math.isPrimaryNumber(7);
+        assertTrue(result8);
+
+        boolean result9 = Math.isPrimaryNumber(27);
+        assertFalse(result9);
+    }
+
+    @Test
+    public void testExponent() {
+        int result = Math.exp(0);
+        assertEquals(1, result);
+
+        result = Math.exp(1);
+        assertEquals(1, result);
+
+        result = Math.exp(2);
+        assertEquals(2, result);
+
+        result = Math.exp(3);
+        assertEquals(6, result);
+
+        result = Math.exp(9);
+        assertEquals(362880, result);
+    }
+
+    @Test
+    public void testTemperature() {
+        boolean result = Math.changeTemperature(16);
+        assertTrue(result);
+
+        result = Math.changeTemperature(17);
+        assertTrue(result);
+
+        result = Math.changeTemperature(15);
+        assertFalse(result);
+
+        result = Math.changeTemperature(32);
+        assertTrue(result);
+
+        result = Math.changeTemperature(33);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testLogin() {
+        boolean result = Math.login("Ducdt@123", "123456");
+        assertTrue(result);
+
+        result = Math.login("  ", "123456");
+        assertFalse(result);
+
+        result = Math.login("  ", " ");
+        assertFalse(result);
+
+        result = Math.login("ducdt", "");
+        assertFalse(result);
+
+        result = Math.login("ducd@t", "123456");
+        assertFalse(result);
     }
 
     @After
